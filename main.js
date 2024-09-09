5f430ec988281d436c464fc9c73c296629ac07d0
ahmed-reda-1 <elawy799@gmail.com>
2024-09-09 14:41:40 +0300

    I Created Main Files

---
+++ main.js
@@ -0,0 +1,18 @@
+var battery = {
+	charged: '0%',
+	cycles: 120,
+    
+};
+
+anime({
+	targets: battery,
+	charged: '100%',
+	cycles: 130,
+	round: 1,
+	easing: 'linear',
+	update: function () {
+		logEl.innerHTML = JSON.stringify(battery);
+	},
+});
+
+console.log(battery);
